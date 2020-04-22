b2f79babf19d688e4078ebab7b6f807161777099
JoshMR94 <joshuamarram@gmail.com>
2020-04-22 18:19:55 +0200

    State and props explained. Changing to hooks in nexts commits

--- src/App.js
+++ src/App.js
@@ -3,17 +3,42 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
-  render() {
+    //Only for extends Component
+    state = {
+        persons: [
+            { name: "Joshua", age: 26 },
+            { name: "Axel", age: 25 },
+            { name: "Míriam", age:26 }
+        ],
+        otherState: 'some other value'
+    }
+
+    switchNameHandler = () => {
+        // console.log("Was clicked!");
+        // DONT DO THIS:this.state.persons[0].name = "Maximiliano";
+        this.setState({
+            persons: [
+                { name: "Joshuamiliano", age: 26 },
+                { name: "Axel", age: 25 },
+                { name: "Míriam", age:300 }
+            ]
+        });
+    }
+
+    render() {
       
-    return (
-        <div className="App">
-            <h1>Hi, I'm a React App!!11!</h1>
-            <p>This is really working!</p>
-            <Person />
-        </div>
-    );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Working'))
-  }
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App!!11!</h1>
+                <p>This is really working!</p>
+                <button onClick={this.switchNameHandler}>Switch name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: Playing videogames</Person>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+        //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Working'))
+    }
 }
 
 export default App;
