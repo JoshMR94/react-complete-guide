b14b7f9220619968df24d0dc474461c712170988
JoshMR94 <joshuamarram@gmail.com>
2020-04-27 17:39:37 +0200

    Added styles with React

--- src/App.js
+++ src/App.js
@@ -36,12 +36,22 @@ class App extends Component {
     }
 
     render() {
+
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer' 
+        };
       
         return (
             <div className="App">
                 <h1>Hi, I'm a React App!!11!</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler.bind(this, 'Joshuamiliano')}>Switch name</button>
+                <button
+                    style={style} 
+                    onClick={this.switchNameHandler.bind(this, 'Joshuamiliano')}>Switch name</button>
                 <Person 
                     name={this.state.persons[0].name} 
                     age={this.state.persons[0].age}
