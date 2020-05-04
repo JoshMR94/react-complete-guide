e1674c8a184d4698c50c623463af012dde6e810e
JoshMR94 <joshuamarram@gmail.com>
2020-05-04 18:18:03 +0200

    Added handler to delete elements in an array

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
         showPersons: false
     }
 
+    /*
     switchNameHandler = (newName) => {
         // console.log("Was clicked!");
         // DONT DO THIS:this.state.persons[0].name = "Maximiliano";
@@ -25,6 +26,7 @@ class App extends Component {
             ]
         });
     }
+    */
 
     nameChangeHandler = (event) => {
         this.setState({
@@ -34,6 +36,12 @@ class App extends Component {
                 { name: "Míriam", age: 26 }
             ]
         });
+    }
+
+    deletePersonHandler = (personIndex) => {
+        const persons = this.state.persons;
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
     }
 
     togglePersonsHandler = () => {
@@ -56,17 +64,12 @@ class App extends Component {
         if(this.state.showPersons) {
             persons = (
                 <div>
-                    <Person 
-                        name={this.state.persons[0].name} 
-                        age={this.state.persons[0].age}
-                        click={this.switchNameHandler.bind(this, '!!!!!!')}
-                        changed={this.nameChangeHandler}>My hobbies: Playing videogames</Person>
-                    <Person 
-                        name={this.state.persons[1].name} 
-                        age={this.state.persons[1].age}/>
-                    <Person 
-                        name={this.state.persons[2].name} 
-                        age={this.state.persons[2].age}/>
+                    {this.state.persons.map((person, index) => {
+                        return <Person 
+                            click={() => this.deletePersonHandler(index)}
+                            name={person.name} 
+                            age={person.age} />
+                    })}
                 </div>
             );
         }   
