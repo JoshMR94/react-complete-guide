ea24089f998c7e4fb844af8645cd259e8d5a0909
JoshMR94 <joshuamarram@gmail.com>
2020-06-09 17:26:36 +0200

    Added Radium, a module used for modifying css functionalities like hover or mediaqueries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
+import Radium from 'radium';
 
 class App extends Component {
     //Only for extends Component
@@ -69,7 +70,11 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer' 
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            } 
         };
 
         let persons = null;
@@ -89,6 +94,10 @@ class App extends Component {
             );
 
             style.backgroundColor = 'red';
+            style[':hover'] = {
+                backgroundColor: 'lightred',
+                color: 'black'
+            } 
         }   
 
         const classes = [];
@@ -115,4 +124,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
